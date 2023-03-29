2068ee4c7999be949f192b178a7510f555fcf56a
luisgrangefe177 <luiscamosquera00@gmail.com>
2023-03-29 02:46:23 -0500

    Cambios de los inicios

--- src/main/resources/static/js/indexAnimation.js
+++ src/main/resources/static/js/indexAnimation.js
@@ -1,8 +1,22 @@
-window.sr = scrollReveal();
+window.sr = ScrollReveal();
 
 sr.reveal('.animation',{
-    duration:1800,
+    duration:3800,
     origin: 'buttom',
+    distance:'-100px',
+    reset:true,
+});
+
+sr.reveal('.animation2',{
+    duration:2500,
+    origin: 'bottom',
+    distance:'-100px',
+    reset:true,
+});
+
+sr.reveal('.animation3',{
+    duration:3800,
+    origin: 'left',
     distance:'-100px',
     reset:true,
 });
