60925e706ab55287e47d884874a20709a5dc5009
Joan <jovenig1@gmail.com>
2023-03-30 13:19:11 -0500

    controladores

--- src/main/java/com/app/web/controllers/ClienteController.java
+++ src/main/java/com/app/web/controllers/ClienteController.java
@@ -1,11 +1,19 @@
 package com.app.web.controllers;
 
+import com.app.web.servicios.CalificacionServicio;
+import com.app.web.servicios.PqrsServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClienteController {
+
+	@Autowired
+	private PqrsServicio servicio;
+	@Autowired
+	private CalificacionServicio servicio1;
 
 	@GetMapping("/cliente/Bienvenida")
 	public String bienvenidaCliente (Model modelo) {
@@ -16,7 +24,7 @@ public class ClienteController {
 
 	@GetMapping("/cliente/Calificacion")
 	public String ClienteCalificacion (Model modelo) {
-
+		modelo.addAttribute("Calificacion", servicio1.listarCalificacion());
 		return "/Cliente_Calificacion";
 	}
 
@@ -34,7 +42,7 @@ public class ClienteController {
 
 	@GetMapping("/cliente/Pqrs")
 	public String ClientePqrs (Model modelo) {
-
+		modelo.addAttribute("Pqrs", servicio.listarPQRS());
 		return "/Cliente_Pqrs";
 	}
 
